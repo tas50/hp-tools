819b64b08ecc915dc0881397236d6b6ced1ebf26
Tim Smith <tsmith@llnw.com>
2013-11-17 19:29:49 -0800

    Rubocop fixes

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
-maintainer       "Webtrends Inc"
-maintainer_email "hostedops@webtrends.com"
-license          "Apache 2.0"
-description      "Installs/Configures various HP tools"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.3.0"
+name             'hp-tools'
+maintainer       'Webtrends Inc'
+maintainer_email 'hostedops@webtrends.com'
+license          'Apache 2.0'
+description      'Installs/Configures various HP tools'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.3.0'
 
 %w{ centos redhat fedora amazon scientific oracle }.each do |os|
   supports os
