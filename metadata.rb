ebe7dace539cf5a4b7cfe0fea221344c96a26f0b
Tim Smith <tsmith@llnw.com>
2013-11-17 19:34:00 -0800

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'hostedops@webtrends.com'
 license          'Apache 2.0'
 description      'Installs/Configures various HP tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '1.0.0'
 
 %w{ centos redhat fedora amazon scientific oracle }.each do |os|
   supports os
