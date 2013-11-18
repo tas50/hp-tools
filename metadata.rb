e119667d670eb8d801aaf158fd00586e3cae6733
Tim Smith <tsmith@llnw.com>
2013-11-17 19:21:29 -0800

    Add support for oracle linux to the metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ description      "Installs/Configures various HP tools"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.3.0"
 
-%w{ centos redhat fedora amazon scientific }.each do |os|
+%w{ centos redhat fedora amazon scientific oracle }.each do |os|
   supports os
 end
