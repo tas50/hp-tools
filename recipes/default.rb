a0b14f4c8c5be591a08d0a0838d571138b53767b
Tim Smith <tsmith84@gmail.com>
2015-09-05 17:36:54 -0700

    Update the copyright for Webtrends

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: hp-tools
 # Recipe:: default
 #
-# Copyright 2012, Webtrends
+# Copyright 2012, Webtrends Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
